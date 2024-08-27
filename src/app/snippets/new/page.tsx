59e4911dfebd244bcf2ab35753b311533af1a32a
ivangguz <ivangguz@gmail.com>
2024-08-26 22:48:07 -0600

    Cambios finales

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { redirect } from "next/navigation";
+import Inputs from "@/components/Inputs";
 
 export default function SnippetCreatePage() {
   async function createSnippet(formData: FormData) {
@@ -28,27 +29,9 @@ export default function SnippetCreatePage() {
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         {/* Primer Input */}
-        <div className="flex gap-4">
-          <label className="w-12" htmlFor="title">
-            Title
-          </label>
-          <input
-            name="title"
-            className="border rounded p-2 w-full"
-            id="title"
-          />
-        </div>
+        <Inputs tag="title">Title</Inputs>
         {/* Segundo Input */}
-        <div className="flex gap-4">
-          <label className="w-12" htmlFor="code">
-            Code
-          </label>
-          <textarea
-            name="code"
-            className="border rounded p-2 w-full"
-            id="code"
-          />
-        </div>
+        <Inputs tag="code" as="textarea">Code</Inputs>
         <button
           type="submit"
           className=" rounded p-2 bg-blue-200 my-2 w-20 mx-auto"
--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,3 +1,25 @@
-export default function Inputs(){
-    
+import React from 'react';
+
+interface InputsProps {
+  children: React.ReactNode; // Define el tipo de `children`
+  tag: string;
+  as?: 'input' | 'textarea'; // Prop para decidir qué tipo de elemento renderizar
+}
+
+export default function Inputs({ children, tag, as = 'input' }: InputsProps) {
+  const Element = as; // Usa `as` para determinar el tipo de elemento a renderizar
+
+  return (
+    <div className="flex gap-4">
+      <label className="w-12" htmlFor={tag}>
+        {children}
+      </label>
+      <Element
+        name={tag}
+        className="border rounded p-2 w-full"
+        id={tag}
+        // Puedes agregar otras props aquí si es necesario
+      />
+    </div>
+  );
 }
